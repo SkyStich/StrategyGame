2ff51047678bb1fe3ea3fdcc1e2441c52a1f9198
Stich <SkyandSmurf@gmail.com>
2021-12-11 22:09:58 +0300

    Create Strategy HUD

--- Source/StrategyGame/Private/Player/HUD/StrategyGameBaseHUD.cpp
+++ Source/StrategyGame/Private/Player/HUD/StrategyGameBaseHUD.cpp
@@ -2,4 +2,51 @@
 
 
 #include "Player/HUD/StrategyGameBaseHUD.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Blueprint/UserWidget.h"
+#include "BlueprintFunctionLibraries/SyncLoadLibrary.h"
 
+AStrategyGameBaseHUD::AStrategyGameBaseHUD()
+{
+	static ConstructorHelpers::FClassFinder<UUserWidget>BuildingGridFinder(TEXT("/Game/Blueprints/UI/Building/W_BuildingGrid"));
+	if(BuildingGridFinder.Succeeded()) BuildingGridClassPtr = BuildingGridFinder.Class;	
+}
+
+void AStrategyGameBaseHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CreateBuildingGrid();
+	GetOwningPlayerController()->GetOnNewPawnNotifier().AddUObject(this, &AStrategyGameBaseHUD::OnNewControlledPawn);
+}
+
+void AStrategyGameBaseHUD::OnNewControlledPawn(APawn* NewPawn)
+{
+	if(GetOwningPlayerController()->GetSpectatorPawn())
+	{
+		/** if controllered spectator */
+		CreateBuildingGrid();
+	}
+	else if(NewPawn)
+	{
+		/** if player controlled AI Pawn */
+	}
+}
+
+void AStrategyGameBaseHUD::CreateBuildingGrid()
+{
+	if(!BuildingGridWidget)
+	{
+		BuildingGridWidget = USyncLoadLibrary::SyncLoadWidget<UUserWidget>(this, BuildingGridClassPtr, GetOwningPlayerController());
+		if(BuildingGridWidget) BuildingGridWidget->AddToViewport();
+	}
+}
+
+void AStrategyGameBaseHUD::RemoveBuildingGrid()
+{
+	if(BuildingGridWidget)
+	{
+		BuildingGridWidget->RemoveFromParent();
+		BuildingGridWidget = nullptr;
+	}
+}
--- Source/StrategyGame/Public/Player/HUD/StrategyGameBaseHUD.h
+++ Source/StrategyGame/Public/Player/HUD/StrategyGameBaseHUD.h
@@ -4,14 +4,36 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/HUD.h"
+#include "Blueprint/UserWidget.h"
 #include "StrategyGameBaseHUD.generated.h"
 
 /**
  * 
  */
-UCLASS()
+UCLASS(BlueprintType)
 class STRATEGYGAME_API AStrategyGameBaseHUD : public AHUD
 {
 	GENERATED_BODY()
+
+public:
+
+	AStrategyGameBaseHUD();
 	
+protected:
+
+	virtual void BeginPlay() override;
+
+	virtual void CreateBuildingGrid();
+	virtual void RemoveBuildingGrid();
+
+	UFUNCTION()
+	virtual void OnNewControlledPawn(APawn* NewPawn);
+
+protected:
+
+	UPROPERTY()
+	TAssetSubclassOf<UUserWidget> BuildingGridClassPtr;
+
+	UPROPERTY()
+	UUserWidget* BuildingGridWidget;
 };
