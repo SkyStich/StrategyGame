6a7cf7d68eb2bab12f37ab308a46d7524f4645a8
Stich <SkyandSmurf@gmail.com>
2021-12-11 22:12:34 +0300

    Add stracts for selected objects

---
+++ Source/StrategyGame/Public/Structs/SelectedObjectInfoBase.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "SelectedObjectInfoBase.generated.h"
+
+USTRUCT(BlueprintType)
+struct FSelectedObjectInfo : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	FSelectedObjectInfo() : DisplayName(FText()), Description(FText()), Icon(nullptr) {}
+	FSelectedObjectInfo(const FText& Name, const FText& Desc, TAssetPtr<UTexture2D> Texture) : DisplayName(Name), Description(Desc), Icon(Texture) {}
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SelectedInfo")
+	FText DisplayName;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SelectedInfo")
+	FText Description;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SelectedInfo")
+	TAssetPtr<UTexture2D> Icon;
+};
+
+USTRUCT(BlueprintType)
+struct FBuildingObjectInfo : public FSelectedObjectInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SelectedInfo")
+	TAssetSubclassOf<class ABaseBuildingActor> BuildingClass; 
+};
+
